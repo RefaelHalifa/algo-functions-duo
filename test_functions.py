047ba989c15d9d2136db6f3b5613e792d225e30f
Yehuda_Gabrieli <yehuda8009@gmail.com>
2025-06-19 16:38:04 +0300

    הוסף סקריפט בדיקה מקיף

---
+++ test_functions.py
@@ -0,0 +1,10 @@
+from math_functions import *
+from sequence_functions import *
+from sorting_functions import *
+
+print(atzeret(5))
+print(is_prime(7))
+print(fib(5))
+arr = [1,9,-2,8,3,-7,4,6,5]
+sorting_bubbles_up(arr)
+print(arr)
