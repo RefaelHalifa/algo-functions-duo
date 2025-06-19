3c3f65984a6c8cdb6ccad33eea72979b8063339a
Refael Halifa <rafaelhalifa@gmail.com>
2025-06-19 15:25:10 +0300

    הוספת פונקציה בדיקת מספר ראשוני

--- math_functions.py
+++ math_functions.py
@@ -4,5 +4,15 @@ def welcome():
     """פונקציה ברכה"""
     print("ברוכים הבאים לספרייה של פונקציות אלגוריתמיות")
 
+def is_prime(n, i=2):
+    if n <= 1:
+        return False
+    if i > n // 2:
+        return True
+    if n % i == 0:
+        return False
+    return is_prime(n, i + 1)
+
 if __name__ == "__main__":
     welcome()
+    print(is_prime(7))
