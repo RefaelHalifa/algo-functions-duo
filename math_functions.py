242f0cab7ed95e8da634a8f294d7e417aacff5d3
Yehuda_Gabrieli <yehuda8009@gmail.com>
2025-06-19 15:22:33 +0300

    הוספת פונקיית פקטוריאל

--- math_functions.py
+++ math_functions.py
@@ -4,5 +4,11 @@ def welcome():
     """פונקציה ברכה"""
     print("ברוכים הבאים לספרייה של פונקציות אלגוריתמיות")
 
+def atzeret(num: int):
+    if num == 1:
+        return 1
+    return atzeret(num - 1) * num
+
 if __name__ == "__main__":
     welcome()
+    print(atzeret(5))
