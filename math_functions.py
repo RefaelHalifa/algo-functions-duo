ad7944c9d64504e34c9691ffe2d2d2fdd95c74b6
Yehuda_Gabrieli <yehuda8009@gmail.com>
2025-06-19 15:15:27 +0300

    תיקון פונקציית פקטוריאל

--- math_functions.py
+++ math_functions.py
@@ -3,14 +3,16 @@
 def welcome():
     """פונקציה ברכה"""
     print("ברוכים הבאים לספרייה של פונקציות אלגוריתמיות")
-
-if __name__ == "__main__":
-    welcome()
-    print(atzeret(5))
+    
 def atzeret(num: int):
     if num == 1:
         return 1
     return atzeret(num - 1) * num
 
-print(atzeret(5))
+
+if __name__ == "__main__":
+    welcome()
+    print(atzeret(5))
+
+
 
